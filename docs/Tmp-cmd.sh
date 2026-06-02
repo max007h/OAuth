56dc495f4e1893043b1af3d72522ca80a05a52b1
max007h <janbar.project@gmail.com>
2026-06-02 17:53:40 +0200

    Update Tmp-cmd.sh

--- docs/Tmp-cmd.sh
+++ docs/Tmp-cmd.sh
@@ -1,1 +1,11 @@
-
+docker exec env-pingdirectory-1 \
+  /opt/out/instance/bin/ldapsearch \
+  --hostname localhost \
+  --port 1636 \
+  --useSSL \
+  --trustAll \
+  --bindDN "cn=administrator" \
+  --bindPassword "2FederateM0re" \
+  --baseDN "ou=people,dc=example,dc=com" \
+  "(uid=thomas.martin)" \
+  uid isMemberOf
