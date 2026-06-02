e7097e4d556b21e9d5fd86ca2d77c2ebde8698de
max007h <janbar.project@gmail.com>
2026-06-02 20:26:14 +0200

    Add command to display OAuth client XML file

--- docs/Tmp-cmd.sh
+++ docs/Tmp-cmd.sh
@@ -1,1 +1,3 @@
 docker exec env-pingfederate-1 grep -r "puma-portal" /opt/out/instance/server/default/data/ 2>/dev/null
+
+docker exec env-pingfederate-1 cat /opt/out/instance/server/default/data/oauth-clients/0/2faf3053c30c13380aa110ff25e4a87bdfa83996.xml
