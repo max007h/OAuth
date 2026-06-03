dc88dc0ef4f8f9e6a16c949edd37da3871e0d903
max007h <janbar.project@gmail.com>
2026-06-03 09:48:12 +0200

    Add LDAP user creation method in Tmp-cmd.sh

--- docs/Tmp-cmd.sh
+++ docs/Tmp-cmd.sh
@@ -8,6 +8,36 @@ import org.springframework.security.oauth2.jwt.JwtException;
 
 curl -k https://localhost:1443/scim/v2/Users \
   -u "cn=administrator,dc=example,dc=com:2FederateM0re"
+
+
+private void createViaLdap(CreateUserRequest req) throws NamingException {
+    Hashtable<String, String> env = new Hashtable<>();
+    env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
+    env.put(Context.PROVIDER_URL, "ldap://" + host + ":" + ldapPort);
+    env.put(Context.SECURITY_AUTHENTICATION, "simple");
+    env.put(Context.SECURITY_PRINCIPAL, adminDn);
+    env.put(Context.SECURITY_CREDENTIALS, adminPassword);
+
+    DirContext ctx = new InitialDirContext(env);
+
+    Attributes attrs = new BasicAttributes(true);
+    BasicAttribute oc = new BasicAttribute("objectClass");
+    oc.add("top");
+    oc.add("person");
+    oc.add("organizationalPerson");
+    oc.add("inetOrgPerson");
+    attrs.put(oc);
+    attrs.put(new BasicAttribute("uid", req.uid()));
+    attrs.put(new BasicAttribute("cn", req.firstName() + " " + req.lastName()));
+    attrs.put(new BasicAttribute("sn", req.lastName()));
+    attrs.put(new BasicAttribute("givenName", req.firstName()));
+    attrs.put(new BasicAttribute("mail", req.email()));
+    attrs.put(new BasicAttribute("userPassword", "Changeme123!"));
+
+    String dn = "uid=" + req.uid() + ",ou=people,dc=example,dc=com";
+    ctx.createSubcontext(dn, attrs);
+    ctx.close();
+}
   
 
 
