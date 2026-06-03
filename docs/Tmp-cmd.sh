db13f1a1712ac03083d09c0e4654c189f835c234
max007h <janbar.project@gmail.com>
2026-06-03 09:20:33 +0200

    Update Tmp-cmd.sh

--- docs/Tmp-cmd.sh
+++ docs/Tmp-cmd.sh
@@ -1,3 +1,28 @@
 docker exec env-pingfederate-1 grep -r "puma-portal" /opt/out/instance/server/default/data/ 2>/dev/null
 
 docker exec env-pingfederate-1 cat /opt/out/instance/server/default/data/oauth-clients/0/2faf3053c30c13380aa110ff25e4a87bdfa83996.xml
+
+
+
+@Bean
+public JwtDecoder jwtDecoder() {
+    return token -> {
+        try {
+            SignedJWT jwt = SignedJWT.parse(token);
+            JWTClaimsSet claims = jwt.getJWTClaimsSet();
+            
+            Map<String, Object> headers = jwt.getHeader().toJSONObject();
+            Map<String, Object> claimsMap = claims.toJSONObject();
+            
+            return new Jwt(
+                token,
+                claims.getIssueTime().toInstant(),
+                claims.getExpirationTime().toInstant(),
+                headers,
+                claimsMap
+            );
+        } catch (Exception e) {
+            throw new JwtException("Parse error: " + e.getMessage());
+        }
+    };
+}
