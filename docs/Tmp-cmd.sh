59ec32d56ac79aa06135f16faa9164854b34ab91
max007h <janbar.project@gmail.com>
2026-06-03 09:22:40 +0200

    Update Tmp-cmd.sh with additional imports

--- docs/Tmp-cmd.sh
+++ docs/Tmp-cmd.sh
@@ -1,7 +1,10 @@
 docker exec env-pingfederate-1 grep -r "puma-portal" /opt/out/instance/server/default/data/ 2>/dev/null
 
 docker exec env-pingfederate-1 cat /opt/out/instance/server/default/data/oauth-clients/0/2faf3053c30c13380aa110ff25e4a87bdfa83996.xml
-
+import com.nimbusds.jwt.SignedJWT;
+import com.nimbusds.jwt.JWTClaimsSet;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtException;
 
 
 @Bean
